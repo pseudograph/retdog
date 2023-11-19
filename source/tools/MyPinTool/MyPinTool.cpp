90856f818b98daa6dae78ae2b3a1d34bec3e55e4
pseudograph <sauravvm237@gmail.com>
2023-11-20 03:25:30 +0800

    broken

--- source/tools/MyPinTool/MyPinTool.cpp
+++ source/tools/MyPinTool/MyPinTool.cpp
@@ -8,14 +8,12 @@
 // Global variables
 /* ================================================================== */
 
-struct Snapshot {
-    ADDRINT ret;
-    ADDRINT bp;
-};
-
 std::stack<ADDRINT> callStack;
 std::stack<ADDRINT> bpStack;
 std::string mainImage;
+
+#define MALLOC_FUNC "malloc"
+#define FREE_FUNC "free"
 
 enum MEMSTATE {
     ACTIVE,
@@ -33,9 +31,6 @@ std::unordered_map<ADDRINT, MEMSTATE> heap;
 // Utilities
 /* ===================================================================== */
 
-/*!
- *  Print out help message.
- */
 INT32 Usage()
 {
     std::cerr << "Monitors control flow and heap allocations for errors." << std::endl;
@@ -90,6 +85,7 @@ VOID logMalloc(ADDRINT* addr) {
         heap[*addr] = ACTIVE;
         return;
     }
+    printf("malloc AT 0x%016lx\n", *addr);
     heap.insert({*addr, ACTIVE});
 }
 
@@ -99,6 +95,11 @@ VOID freeMemory(ADDRINT* addr) {
     } else if (heap.find(*addr) != heap.end() && (heap[*addr] == FREEING || heap[*addr] == FREE)) {
         printf("----------[DOUBLE FREE]----------\n");
         printf("MEMORY AT 0x%016lx\n", *addr);
+        if (heap[*addr] == FREEING) {
+            printf("status freeing\n");
+        } else if (heap[*addr] == FREE) {
+            printf("status free");
+        }
         printf("---------------------------------\n");
     } else if (heap.find(*addr) == heap.end()) {
         printf("----------[FREE BEFORE MALLOC]----------\n");
@@ -124,6 +125,7 @@ VOID verifyMemWrite(ADDRINT addr) {
             printf("MEMORY AT 0x%016lx\n", addr);
             printf("--------------------------------------\n");
         } else if (heap[addr] == FREEING) {
+            printf("free AT 0x%016lx\n", addr);
             heap[addr] = FREE;
         }
     }
@@ -134,14 +136,14 @@ VOID verifyMemWrite(ADDRINT addr) {
 /* ===================================================================== */
 
 VOID Image(IMG img, VOID* val) {
-    RTN mallocRtn = RTN_FindByName(img, "malloc");
+    RTN mallocRtn = RTN_FindByName(img, MALLOC_FUNC);
     if (RTN_Valid(mallocRtn)) {
         RTN_Open(mallocRtn);
         RTN_InsertCall(mallocRtn, IPOINT_AFTER, (AFUNPTR) logMalloc, IARG_FUNCRET_EXITPOINT_REFERENCE, IARG_END);
         RTN_Close(mallocRtn);
     }
 
-    RTN freeRtn = RTN_FindByName(img, "free");
+    RTN freeRtn = RTN_FindByName(img, FREE_FUNC);
     if (RTN_Valid(freeRtn)) {
         RTN_Open(freeRtn);
         RTN_InsertCall(freeRtn, IPOINT_BEFORE, (AFUNPTR) freeMemory, IARG_FUNCARG_ENTRYPOINT_REFERENCE, 0,
